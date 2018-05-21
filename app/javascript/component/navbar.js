07420c714ff4bfb50ce6646136466ae48e730a6a
giovanni rescaglio <giovannirescaglio@gmail.com>
2018-05-21 14:12:32 +0200

    avancement mister cocktail front

--- app/javascript/component/navbar.js
+++ app/javascript/component/navbar.js
@@ -5,7 +5,7 @@ function initUpdateNavbarOnScroll() {
   console.log(navbar)
   window.addEventListener('scroll', () => {
     console.log(window.scrollY)
-    if (window.scrollY > 168) {
+    if (window.scrollY > 320) {
       navbar.classList.add('navbar-wagon-white');
     } else {
       navbar.classList.remove('navbar-wagon-white');
